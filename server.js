1b740caec039d7cc4a4a849c1b0dcb3e84260a00
Rishav Maskey <rishav95maskey@gmail.com>
2022-03-16 11:10:10 +0545

    Added compression

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const routes = require("./routes");
+const compression = require('compression');
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
